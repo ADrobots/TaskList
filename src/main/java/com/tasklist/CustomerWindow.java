2c880e1b5167352464119186f7bc8891760cac13
work <vox15v@mail.ru>
2021-05-31 18:06:30 +0500

    add button Send

--- src/main/java/com/tasklist/CustomerWindow.java
+++ src/main/java/com/tasklist/CustomerWindow.java
@@ -11,9 +11,11 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -25,6 +27,15 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class CustomerWindow{
+	
+	private JLabel mailLabelCustomer;
+	private JButton createCustomer;
+	private JButton deleteCustomer;
+	private JButton sendCustomer;
+	private JTextField mailCustomer;
+	private JTextField companyCustomer;
+	private JList jList;
+	private DefaultListModel<String> model;
 	
 	public CustomerWindow() {
 		
@@ -69,8 +80,13 @@ public class CustomerWindow{
 		statusCustomer.setBorder(BorderFactory.createLineBorder(Color.black));
 		statusCustomer.setLayout(new GridLayout(0,1));
 		
-		JTextField jtfMail=new JTextField("mail",10);
-		JTextField jtfCompany=new JTextField("company",10);
+		JPanel southStatusCustomer=new JPanel();
+		southStatusCustomer.setLayout(new BoxLayout(southStatusCustomer, BoxLayout.X_AXIS));
+		southStatusCustomer.setLayout(new GridLayout(0,2));
+		statusCustomer.add(southStatusCustomer, BorderLayout.SOUTH);
+		
+		//JTextField jtfMail=new JTextField("mail",10);
+		//JTextField jtfCompany=new JTextField("company",10);
 		JTextField jtfManager=new JTextField("manager",10);
 		JTextField jtfComments=new JTextField("comments",10);
 		
@@ -78,24 +94,129 @@ public class CustomerWindow{
 		mainPanel.add(listCustomer, BorderLayout.CENTER);
 		mainPanel.add(statusCustomer, BorderLayout.SOUTH);
 		
-		leftAddCustomer.add(jtfMail);
-		leftAddCustomer.add(jtfCompany);
+		mailCustomer=new JTextField("mail",10);
+		leftAddCustomer.add(mailCustomer);
+		companyCustomer=new JTextField("company",10);
+		leftAddCustomer.add(companyCustomer);
 		leftAddCustomer.add(jtfManager);
 		leftAddCustomer.add(jtfComments);
-		rightAddCustomer.add(new JButton("Добавить"));
 		
-		rightAddCustomer.add(new JButton("Удалить"));
+		createCustomer=new JButton("ADD customer");
+		rightAddCustomer.add(createCustomer);
+		createCustomer.addMouseListener(new MouseListener() {
+			
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				model.addElement(mailCustomer.getText()+" "+companyCustomer.getText());
+			}
+
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			
+
+			});
 		
-		northListCustomer.add(new JScrollPane(new JList()));
+		deleteCustomer=new JButton("Delete customer");
+		rightAddCustomer.add(deleteCustomer);
+		deleteCustomer.addMouseListener(new MouseListener() {
+			
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				model.remove(jList.getSelectedIndex());
+			}
+
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			
+
+			});
+		
+		model = new DefaultListModel<String>();
+		jList=new JList(model);
+		northListCustomer.add(new JScrollPane(jList));
 		leftSouthListCustomer.add(new JTextArea(2, 50));
-		rightSouthListCustomer.add(new JButton("Отправить"));
+		
+		sendCustomer=new JButton("Send");
+		rightSouthListCustomer.add(sendCustomer);
+		sendCustomer.addMouseListener(new MouseListener() {
+			
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				//model.remove(jList.getSelectedIndex());
+				
+				System.out.println(model.get(jList.getSelectedIndex()));
+			}
+
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			
+
+			});
 		rightSouthListCustomer.add(new JLabel("Status"));
 		
-		statusCustomer.add(new JLabel("mail"));
+		mailLabelCustomer=new JLabel();
+		mailLabelCustomer.setName(model.get(jList.getSelectedIndex()));
+		statusCustomer.add(mailLabelCustomer);
 		statusCustomer.add(new JLabel("company"));
 		statusCustomer.add(new JLabel("manager"));
 		statusCustomer.add(new JLabel("comments"));
 		
+		southStatusCustomer.add(new JTextField("Find...",10));
+		southStatusCustomer.add(new JButton("Search"));
 		
 		jf.getContentPane().add(mainPanel);
 		
